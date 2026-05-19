ba9f2fa32fc2054d0889a7da97cb8895d4ac6460
Bishrav Shiwakoti <bishravs@gmail.com>
2026-05-19 21:13:01 +0500

    pushing on rendering list

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import Header from "../Section/Footer.jsx"
+
+import "./Home.css"
 // const user = {
 //     name : "Ferrari",
 //     imageurl : "https://cdn.ferrari.com/cms/network/media/img/resize/670e710357a595000f736188-ferrari-f80-lineup-desktop?height=750",
@@ -8,30 +10,45 @@ import Header from "../Section/Footer.jsx"
 
 //condtional rendering
 
-import Admin from "../admin/Admin.jsx"
-import LoginForm from "../registration/LoginForm.jsx"
+// import Admin from "../admin/Admin.jsx"
+// import LoginForm from "../registration/LoginForm.jsx"
 
-const isLoggedIn = true;
+// const isLoggedIn = true;
 
-let content;
+// let content;
 
-if(isLoggedIn){
-  content = < Admin />
-}else{
-  content = < LoginForm />
-}
+// if(isLoggedIn){
+//   content = < Admin />
+// }else{
+//   content = < LoginForm />
+// }
+
+//Renering list datas
+
+// const products = [
+//     {title : 'cabbage' , isfruit:false, id:1},
+//     {title : 'Garlic' , isfruit:false, id:2},
+//     {title : 'Apple' ,isfruit:true, id:3},
+// ]
+// const listItems = products.map(products => 
+//     <li key={products.id} style={{color : products.isfruit ? 'magenta' : 'darkgreen'}}>
+//         {products.title}
+//     </li>
+// )
 
 function Home(){
     return(
         <>
-           <div>
+           {/* <div>
             <h1>Conditional Rendering example</h1>
             {content}
-            </div> 
+            </div>  */}
 
             {/* <h1>{user.name}</h1>
             <img className="avatar"
             src={user.imageurl} alt={'Photo of' + user.name} /> */}
+            <ul>{listItems}</ul>
+            
         </>
     )
 }
